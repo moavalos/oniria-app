ec7c7e5d9673d3239b1ce2f453494a9c244432f9
Pedro Osnaghi <osnaghi.prog@gmail.com>
2025-10-09 18:20:00 -0300

    feat: merge all branches

--- src/app/features/auth/components/LoginForm.tsx
+++ src/app/features/auth/components/LoginForm.tsx
@@ -4,50 +4,49 @@ import { login } from "../services/authService";
 import { useTranslation } from "react-i18next";
 
 export default function LoginForm() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const { t } = useTranslation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { t } = useTranslation();
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            const data = await login(email, password);
-            localStorage.setItem("token", data.token);
-            // redirect
-        } catch {
-            setError(t("login.error"));
-        }
-    };
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const data = await login(email, password);
+      localStorage.setItem("token", data.token);
+      // redirect
+    } catch {
+      setError(t("login.error"));
+    }
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder={t("login.email")}
-                className="w-full px-4 py-2 rounded-lg bg-black/40 border border-white/30 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
-            />
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 min-w-30">
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder={t("login.email")}
+        className="w-full px-4 py-2 rounded-lg bg-black/40 border border-white/30 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
 
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder={t("login.password")}
-                className="w-full px-4 py-2 rounded-lg bg-black/40 border border-white/30 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
-            />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder={t("login.password")}
+        className="w-full px-4 py-2 rounded-lg bg-black/40 border border-white/30 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
 
-            <a
-                href="/forgot-password"
-                className="text-pink-500 text-sm self-end hover:underline"
-            >
-                {t("login.forgot")}
-            </a>
+      <a
+        href="/forgot-password"
+        className="text-pink-500 text-sm self-end hover:underline"
+      >
+        {t("login.forgot")}
+      </a>
 
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-            <Button type="submit">{t("login.button")}</Button>
-
-        </form>
-    );
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+      <Button type="submit">{t("login.button")}</Button>
+    </form>
+  );
 }
--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -7,27 +7,27 @@ import bgImage from "../../assets/images/Screenshot_1.png";
 import { useTranslation } from "react-i18next";
 
 export default function Login() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <div className="flex justify-center items-center min-h-screen">
-            <img
-                src={bgImage}
-                alt="background"
-                className="absolute inset-0 w-full h-full object-cover -z-10"
-            />
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <img
+        src={bgImage}
+        alt="background"
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+      />
 
-            <Card>
-                <AuthTitle title="Oniria" subtitle={t("login.subtitle")} />
-                <LoginForm />
-                <div className="flex items-center gap-2 my-4 text-gray-400">
-                    <div className="flex-1 h-px bg-gray-600" />
-                    <span>{t("login.or")}</span>
-                    <div className="flex-1 h-px bg-gray-600" />
-                </div>
-                <GoogleButton />
-                <AuthLinks href="/register" />
-            </Card>
+      <Card className="p-7 min-w-sm">
+        <AuthTitle title="Oniria" subtitle={t("login.subtitle")} />
+        <LoginForm />
+        <div className="flex items-center gap-2 my-4 text-gray-400">
+          <div className="flex-1 h-px bg-gray-600" />
+          <span>{t("login.or")}</span>
+          <div className="flex-1 h-px bg-gray-600" />
         </div>
-    );
+        <GoogleButton />
+        <AuthLinks href="/register" />
+      </Card>
+    </div>
+  );
 }
