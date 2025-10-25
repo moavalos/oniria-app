e973515146006d4bed060d79b7f3758c8914d1ba
moavalos <mora_m_avalos@hotmail.com>
2025-10-25 12:18:07 -0300

    estilos en login y register + toggle traduccion

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -2,32 +2,43 @@ import AuthTitle from "@features/auth/components/AuthTitle";
 import LoginForm from "@features/auth/components/LoginForm";
 import GoogleButton from "@features/auth/components/GoogleButton";
 import AuthLinks from "@features/auth/components/AuthLinks";
-import Card from "@/app/shared/components/Card";
 import bgImage from "@assets/images/Screenshot_1.png";
 
 import { useTranslation } from "react-i18next";
+import HudMenu from "@/app/shared/components/menu/CardMenu";
 
 export default function Login() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <HudMenu.Root className="relative flex justify-center items-center min-h-screen">
       <img
         src={bgImage}
         alt="background"
         className="absolute inset-0 w-full h-full object-cover"
       />
-      <Card className="p-7 min-w-sm">
-        <AuthTitle title="Oniria" subtitle={t("login.subtitle")} />
-        <LoginForm />
-        <div className="flex items-center gap-2 my-4 text-gray-400">
-          <div className="flex-1 h-px bg-gray-600" />
-          <span>{t("login.or")}</span>
-          <div className="flex-1 h-px bg-gray-600" />
-        </div>
-        <GoogleButton />
-        <AuthLinks href="/register" section="login" />
-      </Card>
-    </div>
+
+      <HudMenu.Container className="relative z-10 p-7 min-w-sm max-w-md w-full">
+        <HudMenu.Header>
+          <AuthTitle title="Oniria" subtitle={t("login.subtitle")} />
+        </HudMenu.Header>
+
+        <HudMenu.Body>
+          <LoginForm />
+
+          <div className="flex items-center gap-2 my-4 text-gray-400">
+            <div className="flex-1 h-px bg-gray-600" />
+            <span>{t("login.or")}</span>
+            <div className="flex-1 h-px bg-gray-600" />
+          </div>
+
+          <GoogleButton />
+        </HudMenu.Body>
+
+        <HudMenu.Footer>
+          <AuthLinks href="/register" section="login" />
+        </HudMenu.Footer>
+      </HudMenu.Container>
+    </HudMenu.Root>
   );
 }
--- src/app/pages/register/Register.tsx
+++ src/app/pages/register/Register.tsx
@@ -1,26 +1,34 @@
-import Card from "@/app/shared/components/Card";
 import AuthTitle from "@/app/features/auth/components/AuthTitle";
 import RegisterForm from "@/app/features/auth/components/RegisterForm";
 import AuthLinks from "@/app/features/auth/components/AuthLinks";
 import bgImage from "@assets/images/Screenshot_1.png";
 import { useTranslation } from "react-i18next";
+import HudMenu from "@/app/shared/components/menu/CardMenu";
 
 export default function Register() {
   const { t } = useTranslation();
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
+    <HudMenu.Root className="relative flex justify-center items-center min-h-screen">
       <img
         src={bgImage}
         alt="background"
         className="absolute inset-0 w-full h-full object-cover"
       />
 
-      <Card className="p-7 min-w-sm">
-        <AuthTitle title="Oniria" subtitle={t("register.subtitle")} />
-        <RegisterForm />
-        <AuthLinks href="/" section="register" />
-      </Card>
-    </div>
+      <HudMenu.Container className="relative z-10 p-7 min-w-sm max-w-md w-full">
+        <HudMenu.Header>
+          <AuthTitle title="Oniria" subtitle={t('register.subtitle')} />
+        </HudMenu.Header>
+
+        <HudMenu.Body>
+          <RegisterForm />
+        </HudMenu.Body>
+
+        <HudMenu.Footer>
+          <AuthLinks href="/" section="register" />
+        </HudMenu.Footer>
+      </HudMenu.Container>
+    </HudMenu.Root>
   );
 }
